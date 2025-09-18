2f33f954ffad3b29ea10c690ba96a442d0caee7f
BredeFj <bredefjellanger@gmail.com>
2025-09-18 11:52:08 +0200

    comp code

--- passwordprotector.py
+++ passwordprotector.py
@@ -1,17 +1,18 @@
 # Password Protector - startkode
 import hashlib
 
-# Funksjon som skal hashe et passord
-def hash_password(password):
-    # fullfør koden her
-    return ??? #fullfør koden her
 
-# Main program starter her
+def hash_password(password):
+    
+    encoded_password = hashlib.sha256(password.encode())
+    hashed_password = encoded_password.hexdigest()
+    return hashed_password
+
 password = input("Skriv inn et passord: ")
 
-# Kall funksjonen for å hashe passordet
+
 hashed_password = hash_password(password)
 
-# Output
+
 print("Originalt passord:", password)
 print("Hashet passord:", hashed_password)
